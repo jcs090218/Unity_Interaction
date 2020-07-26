bc4d2c2360ba06f94b00dbda655f4a8588ad2468
jenchieh <jcs090218@gmail.com>
2020-07-26 16:01:34 +0800

    Add windows.

---
+++ UInt_Windows/README.md
@@ -0,0 +1,5 @@
+# Unity Interaction (Windows C#)
+
+This is the project that tests for interaction between Unity Mono and C# DLL .
+
+* [How to make DLL for Unity](https://www.youtube.com/watch?v=Wy4uu8lMEDA)
---
+++ UInt_Windows/UInt_Windows/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace UInt_Windows
+{
+    public class TestClass
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
